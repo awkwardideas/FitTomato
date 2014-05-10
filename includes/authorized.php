3e01ced8e05d5aadf36fddeb7695fb35804aaa8d
Chad Haney <chad@awkwardideas.com>
2014-05-10 00:35:29 -0600

    Changed button sizes to small

--- includes/authorized.php
+++ includes/authorized.php
@@ -1,15 +1,15 @@
 <div class="row">
     <div class="col-md-12 col-lg-10 col-lg-offset-1">
         <div class="row">
-            <div class="col-sm-12 pull-center">
+            <div class="col-xs-12 pull-center">
                 <div class="btn-group options" data-toggle="buttons">
-                    <label class="btn btn-lg btn-info active">
+                    <label class="btn btn-sm btn-info active">
                         <input type="radio" name="options" id="pomodoro"  data-time-value="1500"> Pomodoro
                     </label>
-                    <label class="btn btn-lg btn-info">
+                    <label class="btn btn-sm btn-info">
                         <input type="radio" name="options" id="shortBreak" data-time-value="300"> Short Break
                     </label>
-                    <label class="btn btn-lg btn-info">
+                    <label class="btn btn-sm btn-info">
                         <input type="radio" name="options" id="longBreak" data-time-value="1800"> Long Break
                     </label>
                 </div>
@@ -17,19 +17,19 @@
         </div>
         <br/>
         <div class="row">
-            <div class="col-sm-12 pull-center">
+            <div class="col-xs-12 pull-center">
                 <div class="countDown"></div>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-12 pull-center">
-                <button type="button" class="btn btn-lg btn-success" onclick="startTimer()">
+            <div class="col-xs-12 pull-center">
+                <button type="button" class="btn btn-sm btn-success" onclick="startTimer()">
                     <span class="glyphicon glyphicon-play"></span> Start
                 </button>
-                <button type="button" class="btn btn-lg btn-danger" onclick="stopTimer()">
+                <button type="button" class="btn btn-sm btn-danger" onclick="stopTimer()">
                     <span class="glyphicon glyphicon-stop"></span>  Stop
                 </button>
-                <button type="button" class="btn btn-lg btn-default" onclick="resetTimer()">
+                <button type="button" class="btn btn-sm btn-default" onclick="resetTimer()">
                     <span class="glyphicon glyphicon-stop"></span>  Reset
                 </button>
                 <span id="syncWatch" class="badge palette-alizarin" data-toggle="tooltip" data-trigger="hover" title="The alarm has not synced with your fitbit device.">
